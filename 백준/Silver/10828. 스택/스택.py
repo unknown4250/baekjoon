e4deb8a6a2b2f3cbe18000c65f1212dc6ac18043
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-06-16 12:47:04 +0900

    [Silver IV] Title: 스택, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+
+for _ in range(n):
+    cmd = input().split()
+
+    if cmd[0] == "push":
+        stack.append(int(cmd[1]))
+    
+    elif cmd[0] == "pop":
+        if len(stack) > 0: 
+            print(stack[-1])
+            del(stack[-1])
+        else: 
+            print(-1)
+
+    elif cmd[0] == "size":
+        print(len(stack))
+
+    elif cmd[0] == "empty":
+        if len(stack) == 0: print(1)
+        else:               print(0)
+
+    else: # top
+        if len(stack) > 0:  print(stack[-1]) 
+        else:               print(-1)
