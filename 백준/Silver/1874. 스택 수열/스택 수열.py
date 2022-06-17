b86acba2533fd7c579440ff3baae517543d8bde7
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-06-17 10:53:29 +0900

    [Silver II] Title: 스택 수열, Time: 224 ms, Memory: 32424 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+
+n = int(input())
+
+stack = []
+cnt = 0
+top = False
+
+res = []
+
+
+for _ in range(n):
+    num = int(input())
+    
+    while cnt < num:
+        cnt += 1
+        stack.append(cnt)
+        res.append("+")
+    
+    if stack[-1] == num:
+        stack.pop()
+        res.append("-")
+    else:
+        print("NO")
+        top = True
+        break
+
+if not top:
+    for i in res:
+        print(i)
