333944826a8553727454541cf1f25e1ad7a75c99
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-06-20 12:24:57 +0900

    [Silver V] Title: 숫자 카드, Time: 540 ms, Memory: 122704 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+own_card = {i for i in list(map(int, input().split()))}
+m = int(input())
+check_card = [i for i in list(map(int, input().split()))]
+
+res = []
+for num in check_card:
+    if num in own_card:
+        res.append("1")
+    else:
+        res.append("0")
+
+print(" ".join(res))
