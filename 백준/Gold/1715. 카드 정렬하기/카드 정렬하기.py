521b3088b2fdb8857773f0d620de81c57572ede6
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-07-12 12:31:05 +0900

    Update 카드 정렬하기.py
    주석 추가

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -3,15 +3,19 @@ import heapq
 n = int(input())
 
 nums = []
+
+# 효율성을 위해 우선순위 큐 사용
 for _ in range(n):
     heapq.heappush(nums, int(input()))
 
 res = 0
 
-for _ in range(n-1):
+for _ in range(n-1
+    # heappop으로 작은 값 2개 꺼내서 더해주기
     sum = heapq.heappop(nums) + heapq.heappop(nums)
     res += sum
 
+    # 이전 카드 묶음 합을 저장
     heapq.heappush(nums, sum)
 
 print(res)
