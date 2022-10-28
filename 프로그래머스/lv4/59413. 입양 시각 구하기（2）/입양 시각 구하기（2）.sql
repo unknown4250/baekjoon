526c9e9cfc7502ea4368398b9aeffb5baaa67b74
unknown4250 <mjjj4300@gmail.com>
2022-10-28 16:46:01 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,10 +1,21 @@
-WITH RECURSIVE TIME AS (
-    SELECT 0 AS HOUR 
-    UNION ALL
-    SELECT HOUR+1 FROM TIME WHERE HOUR < 23
+/*
+WITH RECURSIVE(재귀 쿼리) : 가상의 테이블 저장
+
+*/
+WITH RECURSIVE TEMP AS (
+    /* non-recursive 문장 : 첫 번째 루프에서만 실행*/
+    SELECT 0 HOUR
+    UNION
+    /* recursive 문장 */
+    SELECT HOUR + 1 FROM TEMP WHERE HOUR < 23
 )
 
-SELECT HOUR, COUNT(ANIMAL_ID) AS COUNT
-FROM TIME
-LEFT OUTER JOIN ANIMAL_OUTS ON (HOUR=DATE_FORMAT(DATETIME, '%H'))
-GROUP BY HOUR;
+SELECT HOUR, IFNULL(C.COUNT, 0)
+FROM TEMP 
+NATURAL LEFT OUTER JOIN (
+                        SELECT HOUR(DATETIME) HOUR, COUNT(*) COUNT
+                        FROM ANIMAL_OUTS
+                        GROUP BY HOUR
+                        ORDER BY HOUR
+) C
+;
