73477bce83324d0ff01fa02c6bcd2c46fae22146
unknown4250 <mjjj4300@gmail.com>
2022-11-10 15:40:04 +0900

    Update 삼총사.py
    브루트 포스 (3중 for문) 버전에 조합을 이용한 버전 추가

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,10 +1,17 @@
 def solution(number):
     answer = 0
     
+    """
+    # brute-force
     for i in range(len(number)-2):
         for j in range(i+1, len(number)-1):
             for k in range(j+1, len(number)):
                 if number[i] + number[j] + number[k] == 0:
                     answer += 1
+    """
+    
+    for combi in itertools.combinations(number, 3):
+        if sum(combi) == 0:
+            answer += 1
         
     return answer
